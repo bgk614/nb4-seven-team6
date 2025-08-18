b3d012b20493ae08e21624d6927b84bf1713d768
Bogyeong Kim <bgk614@gmail.com>
2025-08-18 18:05:58 +0900

    chore: updates

--- src/middleware/index.js
+++
@@ -1,4 +0,0 @@
-// auth.middleware.ts
-// export { auth } from './auth.middleware.ts';
-export { validate } from './validate.middleware.ts';
-// export { errer } from './error.middleware.ts';
--- src/routes/group.routes.ts
+++ src/routes/group.routes.ts
@@ -2,16 +2,14 @@ import express from 'express';
 
 import { getGroupsController } from '../controllers/group/get_group.controller';
 import { getGroupByIdController } from '../controllers/group/get_group_by_id.controller';
-import { recommendGroup } from '../controllers/group/recommend_group.controller.js';
-import { joinGroup } from '../controllers/group/join_group.controller.js';
-import { leaveGroup } from '../controllers/group/leave_group.controller.js';
+import { recommendGroup } from '../controllers/group/recommend_group.controller';
+import { joinGroup } from '../controllers/group/join_group.controller';
+import { leaveGroup } from '../controllers/group/leave_group.controller';
 import { createGroupController } from '../controllers/group/create_group.controller.ts';
-import { createGroupService } from '../services/group/create_group.service.ts';
 import { validateGroupQuery, validateID } from '../middleware/group.middleware';
 import * as Params from '../models/group/index';
 import * as Controller from '../controllers/group/index';
-import { validate } from '../middleware/index';
-
+import { validate } from '../middleware/validate.middleware';
 export const groupRouter = express.Router();
 
 // 그룹 목록/상세 조회
@@ -19,14 +17,14 @@ groupRouter
   .route('/')
   .get(validateGroupQuery, getGroupsController) // 목록 조회
   .post(validate(Params.CreateGroupSchema), Controller.createGroupController);
-  
+
 groupRouter.get('/:id', validateID, getGroupByIdController); // 상세 조회
+
+groupRouter.post('/:groupId/recommend', recommendGroup); // 추천수 증가
+groupRouter.post('/:groupId/join', joinGroup); // 그룹 참여
+groupRouter.post('/:groupId/leave', leaveGroup); // 그룹 탈퇴
 
 groupRouter
   .route('/:groupId')
   .patch(validate(Params.UpdateGroupSchema), Controller.updateGroupController)
   .delete(Controller.deleteGroupController);
-
-groupRouter.post('/:groupId/recommend', recommendGroup);
-groupRouter.post('/:groupId/join', joinGroup);
-groupRouter.post('/:groupId/leave', leaveGroup);
