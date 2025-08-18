8f26ee3a8b752054b1e35bc2fbd0c28e8eda18ba
Bogyeong Kim <bgk614@gmail.com>
2025-08-18 16:24:25 +0900

    chore: update setup

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,38 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from './swagger.json';
+import SwaggerParser from '@apidevtools/swagger-parser';
+import path from 'path';
+import { fileURLToPath } from 'url'; 
 import { groupRouter } from './routes/group.routes';
+import { uploadRouter } from './routes/upload.route';
+import { errorMiddleware } from './middleware/error.middleware';
+
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const app = express();
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
+app.use('/uploads', express.static('uploads'));
 app.use('/groups', groupRouter);
+app.use('/uploads', uploadRouter);
 
-const port = process.env.PORT ?? '5432';
+app.use(errorMiddleware);
 
-app.listen(port, () => {
-  console.log(`${port} 포트에서 서버 시작`);
-});
+(async () => {
+  try {
+    const swaggerPath = path.join(__dirname, 'swagger', 'swagger.yaml');
+
+    const api = await SwaggerParser.dereference(swaggerPath);
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(api));
+
+    const port = process.env.PORT ?? '6123';
+    app.listen(port, () => {
+      console.log(`Server running at http://localhost:${port}/api-docs`);
+    });
+  } catch (err) {
+    console.error('Swagger load error:', err);
+  }
+})();
