810a6e3d4abdb8162876d27e032674aa424deb9b
ammgree <kruhm5745@gmail.com>
2025-08-19 14:40:49 +0900

    Merge branch 'main' into main

--- src/services/group/get_group_by_id.service.ts
+++ src/services/group/get_group_by_id.service.ts
@@ -29,7 +29,7 @@ export async function getGroupByIdService(groupId: number) {
         },
         badges: {
           select: {
-            type: true,
+            name: true,
           },
         },
       },
