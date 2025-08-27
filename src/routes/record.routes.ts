b91e9e1cbcad21e5307c03dc7f0a50bf5dd9f92b
Bogyeong Kim <bgk614@gmail.com>
2025-08-27 12:12:04 +0900

    fix: record routes update

--- src/routes/record.routes.ts
+++ src/routes/record.routes.ts
@@ -10,9 +10,10 @@ import {
 const r = Router();
 
 // 그룹별 운동 기록 등록
-r.post('/groups/:groupId/records', registerRecord);
-r.get('/groups/:groupId/records', getAllRecords); // 모든 기록 조회 (페이지네이션, 정렬)
-// r.get('/groups/:groupId/rank', getRecordRankings); // 랭킹 조회
-r.get('/groups/:groupId/records/:recordId', getRecordById); // 상세 조회
+r.post('/:groupId/records', registerRecord);
+r.get('/:groupId/records', getAllRecords); // 모든 기록 조회 (페이지네이션, 정렬)
+
+// r.get('/:groupId/rank', getRecordRankings); // 랭킹 조회
+r.get('/:groupId/records/:recordId', getRecordById); // 상세 조회
 
 export default r;
