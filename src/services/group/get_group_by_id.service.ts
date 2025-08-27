21136cbbb75aa39a3e8b38d6204d2de0bc00bac7
Bogyeong Kim <bgk614@gmail.com>
2025-08-27 09:06:56 +0900

    fix: GroupResponse 타입에 badges 필드 추가

--- src/services/group/get_group_by_id.service.ts
+++ src/services/group/get_group_by_id.service.ts
@@ -1,42 +1,28 @@
 import { PrismaClient } from '@prisma/client';
+import { GroupResponse } from '@/models/group/group_response.dto.js';
+import { toGroupResponse } from '@/utils/mappers/group.mapper.js';
 
 const prisma = new PrismaClient();
 
 // 그룹 상세 조회
-export async function getGroupByIdService(groupId: number) {
-  try {
-    const group = await prisma.group.findUnique({
-      where: { id: groupId },
-      include: {
-        tags: {
-          select: { name: true },
-        },
-        owner: {
-          select: {
-            id: true,
-            nickname: true,
-            createdAt: true,
-            updatedAt: true,
-          },
-        },
-        participants: {
-          select: {
-            id: true,
-            nickname: true,
-            createdAt: true,
-            updatedAt: true,
-          },
-        },
-        badges: {
-          select: {
-            type: true,
-          },
+export async function getGroupByIdService(
+  groupId: number,
+): Promise<GroupResponse | null> {
+  const group = await prisma.group.findUnique({
+    where: { id: groupId },
+    include: {
+      tags: { select: { name: true } },
+      badges: true,
+      participants: {
+        select: {
+          id: true,
+          nickname: true,
+          createdAt: true,
+          updatedAt: true,
         },
       },
-    });
-    return group;
-  } catch (e) {
-    console.log(e);
-    throw e;
-  }
+    },
+  });
+
+  return group ? toGroupResponse(group) : null;
 }
